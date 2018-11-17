a1c3684f452910cdf1721a501a6056cf0bd2a5d1
Cady <2533841806@qq.com>
2018-11-17 23:51:12 +0800

    init

---
+++ src/main/java/com/example/gqy/hellospringboot/helloWorld/HelloWorld.java
@@ -0,0 +1,26 @@
+package com.example.gqy.hellospringboot.helloWorld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: HelloSpringBoot
+ * @description:
+ * @author: Cady
+ * @create: 2018-11-17 23:04
+ **/
+@RestController
+public class HelloWorld {
+    /**
+    * @Description:
+    * @Param: []
+    * @return: java.lang.String
+    * @Author: Cady
+    * @Date: 2018/11/17
+    */
+    @GetMapping("/helloWorld")
+    public String print(){
+        return "Hello World!";
+    }
+}
